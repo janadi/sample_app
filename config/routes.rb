6a81f83246bf9a1a0f0a5477fa4bb7b8fd38b277
janadi <touwabou@gmx.net>
2012-07-14 17:20:00 +0200

    ch5.3.3 finished and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  	get "static_pages/home"
+  root to: 'static_pages#home', :via => :get
 
-  	get "static_pages/help"
-
-	get "static_pages/about"
-	
-	get "static_pages/contact"
+  match '/help',    to: 'static_pages#help', :via => :get
+  match '/about',   to: 'static_pages#about', :via => :get
+  match '/contact', to: 'static_pages#contact', :via => :get
+  
 	
   # The priority is based upon order of creation:
   # first created -> highest priority.
