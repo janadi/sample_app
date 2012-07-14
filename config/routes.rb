fc96514cd1d452e27260c1c2b5fa5dbb53ae9cfe
janadi <touwabou@gmx.net>
2012-07-14 17:49:42 +0200

    ch 5: finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home', :via => :get
+  get "users/new"
 
-  match '/help',    to: 'static_pages#help', :via => :get
-  match '/about',   to: 'static_pages#about', :via => :get
-  match '/contact', to: 'static_pages#contact', :via => :get
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
   
 	
   # The priority is based upon order of creation:
