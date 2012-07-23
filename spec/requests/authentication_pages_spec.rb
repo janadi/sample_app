d907a5d7f272e9ffee32715c9f3ec7d48bebb4ea
janadi <touwabou@gmx.net>
2012-07-23 19:55:12 +0200

    ch9.1.2 finished, all errors fixed

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,11 +15,15 @@ describe "signin" do
 
     before { visit signin_path }
 
-    describe "with invalid information" do
-      before { click_button "Sign in" }
+	describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-      it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
 
       describe "after visiting another page" do
         before { click_link "Home" }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,7 +53,22 @@ require 'spec_helper'
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-  
+    
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
 
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
   end
+ end
 end
