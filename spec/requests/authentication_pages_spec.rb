0ef616919763127b2ffc6e82ec2011015135dbb1
janadi <touwabou@gmx.net>
2012-07-23 20:23:12 +0200

    ch9.2.2 finished, no errors

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Authentication" do
     it { should have_selector('title', text: 'Sign in') }
   end
   
-describe "signin" do
+  describe "signin" do
 
     before { visit signin_path }
 
@@ -49,5 +49,43 @@ describe "signin" do
         it { should have_link('Sign in') }
       end
     end
+  end
+  
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+    
+    # Listing 9.14
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+    ###
   end
 end
